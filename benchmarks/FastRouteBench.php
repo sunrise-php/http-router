4cbbee52324ea26ee910d835083d8b1087e59e3d
fenric <afenric@gmail.com>
2018-12-22 02:40:30 +0300

    Written benchmarks

---
+++ benchmarks/FastRouteBench.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Sunrise\Http\Router\Benchs;
+
+use FastRoute\RouteParser\Std as RouteParser;
+use FastRoute\DataGenerator\GroupCountBased as DataGenerator;
+use FastRoute\Dispatcher\GroupCountBased as Dispatcher;
+use FastRoute\RouteCollector;
+use Sunrise\Http\ServerRequest\ServerRequestFactory;
+
+/**
+ * @BeforeMethods({"init"})
+ */
+class FastRouteBench
+{
+	protected $maxRoutes = 1000;
+	protected $request;
+
+	public function init()
+	{
+		$uri = \sprintf('/route/%d', $this->maxRoutes);
+
+		$this->request = (new ServerRequestFactory)
+		->createServerRequest('GET', $uri);
+	}
+
+	/**
+	 * @Warmup(1)
+	 * @Revs(10)
+	 * @Iterations(100)
+	 */
+	public function benchFastRouteMatch()
+	{
+		$map = new RouteCollector(
+			new RouteParser(),
+			new DataGenerator()
+		);
+
+		for ($i = 1; $i <= $this->maxRoutes; $i++)
+		{
+			$path = \sprintf('/route/%d', $i);
+			$action = function() {};
+
+			$map->get($path, $action);
+		}
+
+		$dispatcher = new Dispatcher($map->getData());
+
+		$dispatcher->dispatch(
+			$this->request->getMethod(),
+			$this->request->getUri()->getPath()
+		);
+	}
+}
---
+++ benchmarks/ZendBench.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Sunrise\Http\Router\Benchs;
+
+use Sunrise\Http\ServerRequest\ServerRequestFactory;
+use Zend\Psr7Bridge\Psr7ServerRequest;
+use Zend\Router\Http\TreeRouteStack;
+
+/**
+ * @BeforeMethods({"init"})
+ */
+class ZendBench
+{
+	protected $maxRoutes = 1000;
+	protected $request;
+
+	public function init()
+	{
+		$uri = \sprintf('/route/%d', $this->maxRoutes);
+
+		$this->request = (new ServerRequestFactory)
+		->createServerRequest('GET', $uri);
+	}
+
+	/**
+	 * @Warmup(1)
+	 * @Revs(10)
+	 * @Iterations(100)
+	 */
+	public function benchZendMatch()
+	{
+		$router = new TreeRouteStack();
+
+		for ($i = 1; $i <= $this->maxRoutes; $i++)
+		{
+			$id = \sprintf('route:%d', $i);
+			$path = \sprintf('/route/%d', $i);
+
+			/**
+			 * @link https://github.com/zendframework/zend-expressive-zendrouter/blob/master/src/ZendRouter.php
+			 */
+			$router->addRoute($id, [
+				'type' => 'segment',
+				'options' => [
+					'route' => $path,
+				],
+				'may_terminate' => false,
+				'child_routes' => [
+					'GET' => [
+						'type' => 'method',
+						'options' => [
+							'verb' => 'GET',
+						],
+					],
+					'method_not_allowed'=> [
+						'type' => 'regex',
+						'priority' => -1,
+						'options' => [
+							'regex' => '',
+							'defaults' => [
+								'method_not_allowed' => $path,
+							],
+							'spec' => '',
+						],
+					],
+				],
+			]);
+		}
+
+		$router->match(Psr7ServerRequest::toZend($this->request, true));
+	}
+}
