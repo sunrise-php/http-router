80a3d70bd35bde87b94a5b22b16148119c0ef2bb
Анатолий Нехай <fenric@iMac-Anatolij.local>
2019-11-25 05:50:34 +0300

    Route factory

---
+++ tests/RouteFactoryTest.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+namespace Sunrise\Http\Router\Tests;
+
+/**
+ * Import classes
+ */
+use PHPUnit\Framework\TestCase;
+use Sunrise\Http\Router\RouteFactory;
+use Sunrise\Http\Router\RouteInterface;
+
+/**
+ * RouteFactoryTest
+ */
+class RouteFactoryTest extends TestCase
+{
+
+    /**
+     * @return void
+     */
+    public function testCreateRoute() : void
+    {
+        $routeName = Fixture\TestRoute::getTestRouteName();
+        $routePath = Fixture\TestRoute::getTestRoutePath();
+        $routeMethods = Fixture\TestRoute::getTestRouteMethods();
+        $routeRequestHandler = Fixture\TestRoute::getTestRouteRequestHandler();
+
+        $route = (new RouteFactory)->createRoute(
+            $routeName,
+            $routePath,
+            $routeMethods,
+            $routeRequestHandler
+        );
+
+        $this->assertInstanceOf(RouteInterface::class, $route);
+
+        $this->assertSame($routeName, $route->getName());
+        $this->assertSame($routePath, $route->getPath());
+        $this->assertSame($routeMethods, $route->getMethods());
+        $this->assertSame($routeRequestHandler, $route->getRequestHandler());
+
+        // default property values...
+        $this->assertSame([], $route->getMiddlewares());
+        $this->assertSame([], $route->getAttributes());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreateRouteWithOptionalParams() : void
+    {
+        $routeName = Fixture\TestRoute::getTestRouteName();
+        $routePath = Fixture\TestRoute::getTestRoutePath();
+        $routeMethods = Fixture\TestRoute::getTestRouteMethods();
+        $routeRequestHandler = Fixture\TestRoute::getTestRouteRequestHandler();
+        $routeMiddlewares = Fixture\TestRoute::getTestRouteMiddlewares();
+        $routeAttributes = Fixture\TestRoute::getTestRouteAttributes();
+
+        $route = (new RouteFactory)->createRoute(
+            $routeName,
+            $routePath,
+            $routeMethods,
+            $routeRequestHandler,
+            $routeMiddlewares,
+            $routeAttributes
+        );
+
+        $this->assertSame($routeName, $route->getName());
+        $this->assertSame($routePath, $route->getPath());
+        $this->assertSame($routeMethods, $route->getMethods());
+        $this->assertSame($routeRequestHandler, $route->getRequestHandler());
+        $this->assertSame($routeMiddlewares, $route->getMiddlewares());
+        $this->assertSame($routeAttributes, $route->getAttributes());
+    }
+}
