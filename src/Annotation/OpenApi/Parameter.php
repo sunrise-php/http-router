8dcb16b3e565395b146013441712bfb96fd08fd6
@fenric <afenric@gmail.com>
2019-12-09 00:19:19 +0300

    style, explode and content fields support

--- src/Annotation/OpenApi/Parameter.php
+++ src/Annotation/OpenApi/Parameter.php
@@ -27,6 +27,8 @@ final class Parameter extends AbstractAnnotation implements ParameterInterface
      * @Enum({"cookie", "header", "query"})
      *
      * @var string
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterin
      */
     public $in;
 
@@ -34,48 +36,90 @@ final class Parameter extends AbstractAnnotation implements ParameterInterface
      * @Required
      *
      * @var string
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parametername
      */
     public $name;
 
     /**
      * @var string
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterdescription
      */
     public $description = '';
 
     /**
      * @var bool
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterrequired
      */
     public $required = false;
 
     /**
      * @var bool
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterdeprecated
      */
     public $deprecated = false;
 
     /**
      * @var bool
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterallowemptyvalue
      */
     public $allowEmptyValue = false;
 
     /**
+     * @Enum({"matrix", "label", "form", "simple", "spaceDelimited", "pipeDelimited", "deepObject"})
+     *
+     * @var string
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterstyle
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#style-values
+     */
+    public $style;
+
+    /**
      * @var bool
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterexplode
+     */
+    public $explode;
+
+    /**
+     * @var bool
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterallowreserved
      */
     public $allowReserved = false;
 
     /**
      * @var \Sunrise\Http\Router\Annotation\OpenApi\SchemaInterface
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterschema
      */
     public $schema;
 
     /**
      * @var mixed
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterexample
      */
     public $example;
 
     /**
      * @var array<\Sunrise\Http\Router\Annotation\OpenApi\ExampleInterface>
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parameterexamples
      */
     public $examples = [];
+
+    /**
+     * @var array<\Sunrise\Http\Router\Annotation\OpenApi\MediaTypeInterface>
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-parametercontent
+     */
+    public $content = [];
 
     /**
      * {@inheritDoc}
@@ -92,6 +136,14 @@ final class Parameter extends AbstractAnnotation implements ParameterInterface
             'allowReserved' => $this->allowReserved,
         ];
 
+        if (isset($this->style)) {
+            $result['style'] = $this->style;
+        }
+
+        if (isset($this->explode)) {
+            $result['explode'] = $this->explode;
+        }
+
         if (isset($this->schema)) {
             $result['schema'] = $this->schema->toArray();
         }
@@ -102,6 +154,10 @@ final class Parameter extends AbstractAnnotation implements ParameterInterface
 
         foreach ($this->examples as $key => $value) {
             $result['examples'][$key] = $value->toArray();
+        }
+
+        foreach ($this->content as $key => $value) {
+            $result['content'][$key] = $value->toArray();
         }
 
         return $result;
