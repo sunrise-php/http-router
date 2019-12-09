67afb443450630d06c26fa2655440a0d261e65f2
@fenric <afenric@gmail.com>
2019-12-09 04:09:53 +0300

    link object support

---
+++ src/Annotation/OpenApi/Link.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+/**
+ * It's free open-source software released under the MIT License.
+ *
+ * @author Anatoly Fenric <anatoly@fenric.ru>
+ * @copyright Copyright (c) 2018, Anatoly Fenric
+ * @license https://github.com/sunrise-php/http-router/blob/master/LICENSE
+ * @link https://github.com/sunrise-php/http-router
+ */
+
+namespace Sunrise\Http\Router\Annotation\OpenApi;
+
+/**
+ * @Annotation
+ *
+ * @Target({"ALL"})
+ *
+ * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#link-object
+ */
+final class Link extends AbstractAnnotation implements LinkInterface
+{
+
+    /**
+     * @var string
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-linkoperationref
+     */
+    public $operationRef;
+
+    /**
+     * @var string
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-linkoperationid
+     */
+    public $operationId;
+
+    /**
+     * @var array
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-linkparameters
+     */
+    public $parameters;
+
+    /**
+     * @var mixed
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-linkrequestbody
+     */
+    public $requestBody;
+
+    /**
+     * @var string
+     *
+     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#user-content-linkdescription
+     */
+    public $description;
+}
---
+++ src/Annotation/OpenApi/LinkReference.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+/**
+ * It's free open-source software released under the MIT License.
+ *
+ * @author Anatoly Fenric <anatoly@fenric.ru>
+ * @copyright Copyright (c) 2018, Anatoly Fenric
+ * @license https://github.com/sunrise-php/http-router/blob/master/LICENSE
+ * @link https://github.com/sunrise-php/http-router
+ */
+
+namespace Sunrise\Http\Router\Annotation\OpenApi;
+
+/**
+ * @Annotation
+ *
+ * @Target({"ANNOTATION"})
+ */
+final class LinkReference extends AbstractReference implements LinkInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAnnotationName() : string
+    {
+        return Link::class;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getComponentName() : string
+    {
+        return 'links';
+    }
+}
