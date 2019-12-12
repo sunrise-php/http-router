daf9e03e2c748de5318d443d13a3949f44cecae7
@fenric <afenric@gmail.com>
2019-12-13 00:47:02 +0300

    improved code

--- src/Annotation/OpenApi/AbstractReference.php
+++ src/Annotation/OpenApi/AbstractReference.php
@@ -9,7 +9,7 @@
  * @link https://github.com/sunrise-php/http-router
  */
 
-namespace Sunrise\Http\Router\Annotation\OpenApi\Reference;
+namespace Sunrise\Http\Router\Annotation\OpenApi;
 
 /**
  * Import classes
@@ -36,7 +36,7 @@ abstract class AbstractReference
     /**
      * @var array
      */
-    private static $objects = [];
+    private static $cache = [];
 
     /**
      * @Required
@@ -63,33 +63,47 @@ abstract class AbstractReference
     /**
      * @return string
      */
-    abstract protected function getTarget() : string;
+    abstract protected function getAnnotationName() : string;
 
     /**
      * @param SimpleAnnotationReader $annotationReader
      *
      * @return null|ComponentObjectInterface
      */
-    public function getComponentObject(SimpleAnnotationReader $annotationReader) : ?ComponentObjectInterface
+    public function getAnnotation(SimpleAnnotationReader $annotationReader) : ?ComponentObjectInterface
     {
-        $key = hash('md5', $this->class . $this->method . $this->property . $this->getTarget());
+        $key = hash(
+            'md5',
+            $this->class .
+            $this->method .
+            $this->property .
+            $this->getAnnotationName()
+        );
 
-        if (isset(self::$objects[$key])) {
-            $this->target = self::$objects[$key];
-            return $this->target;
-        } elseif (isset($this->method)) {
-            $source = new ReflectionMethod($this->class, $this->method);
-            $this->target = $annotationReader->getMethodAnnotation($source, $this->getTarget());
-        } elseif (isset($this->property)) {
-            $source = new ReflectionProperty($this->class, $this->property);
-            $this->target = $annotationReader->getPropertyAnnotation($source, $this->getTarget());
-        } else {
-            $source = new ReflectionClass($this->class);
-            $this->target = $annotationReader->getClassAnnotation($source, $this->getTarget());
+        $this->target =& self::$cache[$key];
+
+        if (isset(self::$cache[$key])) {
+            return self::$cache[$key];
         }
 
-        self::$objects[$key] = $this->target;
-        return $this->target;
+        if (isset($this->method)) {
+            return $this->target = $annotationReader->getMethodAnnotation(
+                new ReflectionMethod($this->class, $this->method),
+                $this->getAnnotationName()
+            );
+        }
+
+        if (isset($this->property)) {
+            return $this->target = $annotationReader->getPropertyAnnotation(
+                new ReflectionProperty($this->class, $this->property),
+                $this->getAnnotationName()
+            );
+        }
+
+        return $this->target = $annotationReader->getClassAnnotation(
+            new ReflectionClass($this->class),
+            $this->getAnnotationName()
+        );
     }
 
     /**
