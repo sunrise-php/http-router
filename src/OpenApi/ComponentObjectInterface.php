aa7722d456cf7a4f03db7366bf1ddf6031b5ea24
@fenric <afenric@gmail.com>
2019-12-12 13:35:07 +0300

    improved logic

--- src/OpenApi/ComponentObjectInterface.php
+++ src/OpenApi/ComponentObjectInterface.php
@@ -14,38 +14,20 @@ namespace Sunrise\Http\Router\OpenApi;
 /**
  * ComponentObjectInterface
  */
-interface ComponentObjectInterface
+interface ComponentObjectInterface extends ObjectInterface
 {
 
     /**
      * Gets a component name
      *
      * @return string
-     *
-     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#fixed-fields-6
      */
     public function getComponentName() : string;
 
     /**
-     * Gets access key for a component object
+     * Gets a reference name
      *
      * @return string
      */
-    public function getObjectAccessKey() : string;
-
-    /**
-     * Gets a component object path
-     *
-     * @return string
-     *
-     * @link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#reference-object-example
-     */
-    public function getObjectPath() : string;
-
-    /**
-     * Gets a component object
-     *
-     * @return ObjectInterface
-     */
-    public function getObject() : ObjectInterface;
+    public function getReferenceName() : string;
 }
