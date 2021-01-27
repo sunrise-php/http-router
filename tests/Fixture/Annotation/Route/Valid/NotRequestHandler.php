d839e4ada65008f60f1252888b5b675576fb0e93
Anatoly Nekhay <afenric@gmail.com>
2021-01-27 06:22:37 +0500

    improved tests

---
+++ tests/Fixture/Annotation/Route/Valid/NotRequestHandler.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Sunrise\Http\Router\Tests\Fixture\Annotation\Route\Valid;
+
+/**
+ * @Route(
+ *   name="test",
+ *   path="/test",
+ *   methods={"GET"},
+ * )
+ */
+class NotRequestHandler
+{
+}
