1cbe7299b49aa7be301303749f651c9607c24874
Anatoly Nekhay <afenric@gmail.com>
2021-10-11 23:41:20 +0500

    improved code

--- src/Command/RouteListCommand.php
+++ src/Command/RouteListCommand.php
@@ -14,6 +14,7 @@ namespace Sunrise\Http\Router\Command;
 /**
  * Import classes
  */
+use RuntimeException;
 use Sunrise\Http\Router\Router;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
@@ -23,31 +24,74 @@ use Symfony\Component\Console\Output\OutputInterface;
 /**
  * Import functions
  */
-use function Sunrise\Http\Router\path_plain;
 use function join;
+use function sprintf;
+use function Sunrise\Http\Router\path_plain;
 
 /**
- * This command displays a list of routes
+ * This command will list all routes in your application
+ *
+ * If you cannot pass the router to the constructor
+ * or your architecture has problems with autowiring,
+ * then just inherit this class and override the getRouter method.
  *
  * @since 2.9.0
  */
-abstract class RouteListCommand extends Command
+class RouteListCommand extends Command
 {
 
     /**
      * {@inheritdoc}
      */
-    public function __construct(?string $name = null)
+    protected static $defaultName = 'router:route-list';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected static $defaultDescription = 'Lists all routes in your application';
+
+    /**
+     * The router instance populated with routes
+     *
+     * @var Router|null
+     */
+    private $router;
+
+    /**
+     * Constructor of the class
+     *
+     * @param Router|null $router
+     */
+    public function __construct(?Router $router = null)
     {
-        parent::__construct($name ?? 'router:route-list');
+        parent::__construct();
+
+        $this->router = $router;
     }
 
     /**
-     * Gets the router filled with routes
+     * Gets the router instance populated with routes
      *
      * @return Router
+     *
+     * @throws RuntimeException
+     *         If the command doesn't contain the router instance.
+     *
+     * @since 2.11.0
      */
-    abstract protected function getRouter() : Router;
+    protected function getRouter() : Router
+    {
+        if (null === $this->router) {
+            throw new RuntimeException(sprintf(
+                'The %2$s() method MUST return the %1$s class instance. ' .
+                'Pass the %1$s class instance to the constructor, or override the %2$s() method.',
+                Router::class,
+                __METHOD__,
+            ));
+        }
+
+        return $this->router;
+    }
 
     /**
      * {@inheritdoc}
