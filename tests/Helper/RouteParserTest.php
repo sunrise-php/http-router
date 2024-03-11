30846ab1f4065359303c27ce0a562de2d78ae4df
Anatoly Nekhay <afenric@gmail.com>
2024-03-11 02:47:51 +0100

    v3

--- tests/Helper/RouteParserTest.php
+++ tests/Helper/RouteParserTest.php
@@ -1,12 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sunrise\Http\Router\Tests\Helper;
 
 use Generator;
+use Sunrise\Http\Router\Exception\InvalidRouteParsingSubjectException;
 use Sunrise\Http\Router\Helper\RouteParser;
 use PHPUnit\Framework\TestCase;
 
-class RouteParserTest extends TestCase
+final class RouteParserTest extends TestCase
 {
     /**
      * @dataProvider validRouteDataProvider
@@ -18,7 +21,18 @@ class RouteParserTest extends TestCase
         $this->assertEquals($expectedVariables, $actualVariables);
     }
 
-    public function validRouteDataProvider(): Generator
+    /**
+     * @dataProvider invalidRouteDataProvider
+     */
+    public function testParseInvalidRoute(string $route, string $expectedMessage): void
+    {
+        $this->expectException(InvalidRouteParsingSubjectException::class);
+        $this->expectExceptionMessageMatches($expectedMessage);
+
+        RouteParser::parseRoute($route);
+    }
+
+    private function validRouteDataProvider(): Generator
     {
         yield [
             '',
@@ -45,6 +59,13 @@ class RouteParserTest extends TestCase
         ];
 
         yield [
+            '/posts(/{id})',
+            [
+                ['name' => 'id', 'optional' => ['left' => '/', 'right' => ''], 'offset' => 8, 'length' => 4],
+            ],
+        ];
+
+        yield [
             '/posts(/{id<\d+>})',
             [
 
@@ -53,18 +74,113 @@ class RouteParserTest extends TestCase
         ];
 
         yield [
-            '(/{lc<[a-z]{2}>})/posts/{id<\d+>}',
+            '/posts(/{id<\d+>}.json)',
             [
-                ['name' => 'lc', 'pattern' => '[a-z]{2}', 'optional' => ['left' => '/', 'right' => ''], 'offset' => 2, 'length' => 14],
-                ['name' => 'id', 'pattern' => '\d+', 'offset' => 24, 'length' => 9],
+                ['name' => 'id', 'pattern' => '\d+', 'optional' => ['left' => '/', 'right' => '.json'], 'offset' => 8, 'length' => 9],
             ],
         ];
 
         yield [
-            '/posts(/{id}.json)',
+            '/posts/{id<\d+>}(.json)',
             [
-                ['name' => 'id', 'optional' => ['left' => '/', 'right' => '.json'], 'offset' => 8, 'length' => 4],
+                ['name' => 'id', 'pattern' => '\d+', 'offset' => 7, 'length' => 9],
             ],
+        ];
+
+        yield [
+            '(/{lang<[a-z]{2}>})/posts(/{id<\d+>})',
+            [
+                ['name' => 'lang', 'pattern' => '[a-z]{2}', 'optional' => ['left' => '/', 'right' => ''], 'offset' => 2, 'length' => 16],
+                ['name' => 'id', 'pattern' => '\d+', 'optional' => ['left' => '/', 'right' => ''], 'offset' => 27, 'length' => 9],
+            ],
+        ];
+    }
+
+    private function invalidRouteDataProvider(): Generator
+    {
+        yield [
+            '((',
+            '/nested optional parts are not supported/',
+        ];
+
+        yield [
+            ')',
+            '/open optional part was not found/',
+        ];
+
+        yield [
+            '{{',
+            '/nested variables are not supported/',
+        ];
+
+        yield [
+            '({x}{',
+            '/more than one variable inside an optional part is not supported/',
+        ];
+
+        yield [
+            '}',
+            '/open variable was not found/',
+        ];
+
+        yield [
+            '{}',
+            '/name is required for its declaration/',
+        ];
+
+        yield [
+            '{<<',
+            '/nested patterns are not supported/',
+        ];
+
+        yield [
+            '{<.><',
+            '/pattern must be preceded by the variable name/',
+        ];
+
+        yield [
+            '{>',
+            '/open pattern was not found/',
+        ];
+
+        yield [
+            '{<>',
+            '/content is required for its declaration/',
+        ];
+
+        yield [
+            '{0',
+            '/variable names cannot start with digits/',
+        ];
+
+        yield [
+            '{!',
+            '/variable names must consist only of digits, letters and underscores/'
+        ];
+
+        yield [
+            '{aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            '/variable names must not exceed 32 characters/'
+        ];
+
+        yield [
+            '/{<#/',
+            '/variable patterns cannot contain the # character/'
+        ];
+
+        yield [
+            '{x<.>!}',
+            '/variable at this position must be closed/'
+        ];
+
+        yield [
+            '(',
+            '/contains an unclosed optional part or variable/'
+        ];
+
+        yield [
+            '{',
+            '/contains an unclosed optional part or variable/'
         ];
     }
 }
