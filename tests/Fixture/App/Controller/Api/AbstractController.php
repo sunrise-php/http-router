9317e540b4c5169d816d3d7ca4fc891588d9215d
Anatoly Nekhay <afenric@gmail.com>
2025-02-10 02:01:33 +0100

    v3

--- tests/Fixture/App/Controller/Api/AbstractController.php
+++ tests/Fixture/App/Controller/Api/AbstractController.php
@@ -7,14 +7,24 @@ namespace Sunrise\Http\Router\Tests\Fixture\App\Controller\Api;
 use Sunrise\Http\Router\Annotation\NamePrefix;
 use Sunrise\Http\Router\Annotation\PathPrefix;
 use Sunrise\Http\Router\OpenApi\Annotation\Operation;
+use Sunrise\Http\Router\OpenApi\Type;
 use Sunrise\Http\Router\Tests\Fixture\App\Controller\AbstractController as BaseAbstractController;
 
 #[PathPrefix('/api')]
 #[NamePrefix('api.')]
 #[Operation([
+    // this is bad, just for testing.
     'externalDocs' => [
         'description' => 'Find more info here',
         'url' => 'https://example.com',
+    ],
+    // this is bad, just for testing.
+    'parameters' => [
+        [
+            'in' => 'header',
+            'name' => 'X-Request-ID',
+            'schema' => new Type(Type::OAS_TYPE_NAME_STRING),
+        ],
     ],
 ])]
 abstract class AbstractController extends BaseAbstractController
