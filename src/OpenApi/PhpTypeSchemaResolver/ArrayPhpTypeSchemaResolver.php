d31884dfbd3ecaa3c28563b2c5cd482257c2352a
Anatoly Nekhay <afenric@gmail.com>
2026-06-15 06:01:11 +0200

    v3.3

--- src/OpenApi/PhpTypeSchemaResolver/ArrayPhpTypeSchemaResolver.php
+++ src/OpenApi/PhpTypeSchemaResolver/ArrayPhpTypeSchemaResolver.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Sunrise\Http\Router\OpenApi\PhpTypeSchemaResolver;
 
+use phpDocumentor\Reflection as PhpDoc;
 use ReflectionAttribute;
 use ReflectionParameter;
 use ReflectionProperty;
@@ -33,6 +34,14 @@ final class ArrayPhpTypeSchemaResolver implements
     OpenApiPhpTypeSchemaResolverManagerAwareInterface
 {
     private readonly OpenApiPhpTypeSchemaResolverManagerInterface $openApiPhpTypeSchemaResolverManager;
+    private readonly PhpDoc\DocBlockFactoryInterface $docBlockFactory;
+    private readonly PhpDoc\Types\ContextFactory $docBlockContextFactory;
+
+    public function __construct()
+    {
+        $this->docBlockFactory = PhpDoc\DocBlockFactory::createInstance();
+        $this->docBlockContextFactory = new PhpDoc\Types\ContextFactory();
+    }
 
     public function setOpenApiPhpTypeSchemaResolverManager(
         OpenApiPhpTypeSchemaResolverManagerInterface $openApiPhpTypeSchemaResolverManager,
@@ -65,13 +74,22 @@ final class ArrayPhpTypeSchemaResolver implements
             if (isset($annotations[0])) {
                 $annotation = $annotations[0]->newInstance();
 
-                $arrayElementPhpType = new Type($annotation->name, $annotation->allowsNull);
-                $arrayElementPhpTypeSchema = $this->openApiPhpTypeSchemaResolverManager
-                    ->resolvePhpTypeSchema($arrayElementPhpType, $phpTypeHolder);
+                $itemPhpType = new Type($annotation->name, $annotation->allowsNull);
+                $phpTypeSchema['items'] = $this->openApiPhpTypeSchemaResolverManager
+                    ->resolvePhpTypeSchema($itemPhpType, $phpTypeHolder);
 
-                $phpTypeSchema['items'] = $arrayElementPhpTypeSchema;
                 if ($annotation->limit !== null) {
                     $phpTypeSchema['maxItems'] = $annotation->limit;
+                }
+            }
+        }
+
+        if ($phpTypeHolder instanceof ReflectionProperty) {
+            if (!isset($phpTypeSchema['items'])) {
+                $itemPhpType = $this->getItemTypeFromDocBlock($phpTypeHolder);
+                if ($itemPhpType !== null) {
+                    $phpTypeSchema['items'] = $this->openApiPhpTypeSchemaResolverManager
+                        ->resolvePhpTypeSchema($itemPhpType, $phpTypeHolder);
                 }
             }
         }
@@ -82,5 +100,79 @@ final class ArrayPhpTypeSchemaResolver implements
     public function getWeight(): int
     {
         return 0;
+    }
+
+    private function getItemTypeFromDocBlock(ReflectionProperty $phpTypeHolder): ?TypeInterface
+    {
+        $docComment = $phpTypeHolder->getDocComment();
+        if ($docComment === false) {
+            return null;
+        }
+
+        $docBlock = $this->docBlockFactory->create(
+            $docComment,
+            $this->docBlockContextFactory->createFromReflector($phpTypeHolder),
+        );
+
+        $varTags = $docBlock->getTagsByName('var');
+        if ($varTags === [] || ! $varTags[0] instanceof PhpDoc\DocBlock\Tags\Var_) {
+            return null;
+        }
+
+        $varType = $varTags[0]->getType();
+        if ($varType === null) {
+            return null;
+        }
+
+        $varType = self::unwrapNullablePhpDocType($varType);
+        if (! $varType instanceof PhpDoc\Types\AbstractList) {
+            return null;
+        }
+
+        $itemType = $varType->getValueType();
+
+        return new Type(
+            name: \ltrim((string) self::unwrapNullablePhpDocType($itemType), '\\'),
+            allowsNull: self::isNullablePhpDocType($itemType),
+        );
+    }
+
+    private static function unwrapNullablePhpDocType(PhpDoc\Type $phpDocType): PhpDoc\Type
+    {
+        if ($phpDocType instanceof PhpDoc\Types\Nullable) {
+            return $phpDocType->getActualType();
+        }
+
+        if ($phpDocType instanceof PhpDoc\Types\Compound) {
+            $types = [];
+            foreach ($phpDocType as $type) {
+                if (! $type instanceof PhpDoc\Types\Null_) {
+                    $types[] = $type;
+                }
+            }
+
+            if (\count($types) === 1) {
+                return $types[0];
+            }
+        }
+
+        return $phpDocType;
+    }
+
+    private static function isNullablePhpDocType(PhpDoc\Type $phpDocType): bool
+    {
+        if ($phpDocType instanceof PhpDoc\Types\Nullable) {
+            return true;
+        }
+
+        if ($phpDocType instanceof PhpDoc\Types\Compound) {
+            foreach ($phpDocType as $type) {
+                if ($type instanceof PhpDoc\Types\Null_) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
